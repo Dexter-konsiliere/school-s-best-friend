86f2456a6500b3493274700c301b2e08b3fa203b
Dexter-konsiliere <vaspokr28@mail.ru>
2025-02-09 13:46:28 +0300

    Update import telebot2;.py

--- main/import telebot2;.py
+++ main/import telebot2;.py
@@ -3,3 +3,10 @@ import sqlite3
 
 bot = telebot.TeleBot('7736265547:AAGnxKHv45qdeeWHlMqrWE_VzGPLCnfl0fw')
 
+@bot.message_handler(commands=['start'])
+def start(message):
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  btn1 = types.KeyboardBatton('Зарегистрироваться')
+  btn2 = types.KeyboardBatton('Авторизоваться')
+  markup.add(btn1, btn2)
+  bot.send_message(message.chat.id, 'Здравствуйте, выберете действие'
