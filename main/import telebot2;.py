5e71ea70e598ff2c760aed6fa1ce86850305bef1
Dexter-konsiliere <vaspokr28@mail.ru>
2025-02-10 19:45:30 +0300

    Update import telebot2;.py

--- main/import telebot2;.py
+++ main/import telebot2;.py
@@ -1,6 +1,12 @@
 import telebot
 import sqlite3
 
+connection = sqlite3.connect('userstable.db')
+cursor = connection.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARU KEY, name TEXT NOT NULL, birth date TEXT NOT NULL, role TEXT NOT NULL)')
+connection.commit()
+connection.close()
+
 bot = telebot.TeleBot('7736265547:AAGnxKHv45qdeeWHlMqrWE_VzGPLCnfl0fw')
 
 @bot.message_handler(commands=['start'])
