3a515740a1bd87c19d0898e92fe9e8503e33c7f6
Dexter-konsiliere <vaspokr28@mail.ru>
2025-02-10 20:47:41 +0300

    Update import telebot2;.py

--- main/import telebot2;.py
+++ main/import telebot2;.py
@@ -1,13 +1,20 @@
 import telebot
 import sqlite3
 
-connection = sqlite3.connect('userstable.db')
+
+connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARU KEY, name TEXT NOT NULL, birth date TEXT NOT NULL, role TEXT NOT NULL)')
+cursor.execute('CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARU KEY, name TEXT NOT NULL, birth date TEXT NOT NULL, role TEXT NOT NULL)')
 connection.commit()
 connection.close()
 
+
 bot = telebot.TeleBot('7736265547:AAGnxKHv45qdeeWHlMqrWE_VzGPLCnfl0fw')
+
+
+name = ''
+birth_date = ''
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -16,3 +23,35 @@ def start(message):
   btn2 = types.KeyboardBatton('Авторизоваться')
   markup.add(btn1, btn2)
   bot.send_message(message.chat.id, 'Здравствуйте! Выберете действие'
+
+                   
+@bot.message_handler(commands=['reg'])
+def reg(message):
+  bot.send_message(message.chat.id, 'Введите своё полное имя')
+  bot.register_next_step_handler(message, user_name)
+
+
+def user_name(message):
+  global name
+  name = message.text.strip()
+  bot.send_message(message.chat.id, 'Введите свою дату рождения в цыфрах')
+  bot.register_next_step_handler(message, user_birth_date)
+
+
+def user_birth_date(message):
+  global birth_date
+  birth_date = message.text.strip()
+  bot.send_message(message.chat.id, 'Вы ученик или учитель?')
+  bot.register_next_step_handler(message, user_role)
+
+
+def user_role(message):
+  role = message.text.strip()
+  bot.send_message(message.chat.id, 'Отлично, вы зарегистрированы!')
+  
+  connection = sqlite3.connect('database.db')
+  cursor = connection.cursor()
+  cursor.execute('INSERT INTO Users (name, birth date, role) VALUES (?, ?, ?)', (name, birth_date, role))
+  connection.commit()
+  connection.close()
+
